07821f44af9d73ca2aeba70596bca236b7ff60e0
sethdanny <naddulidaniel1994@gmail.com>
2022-12-02 01:00:09 +0300

    added task 1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-
 def main(argv):
-    num = len(argv)
-    operators = {
-        '+' : calculator_1.add
-        '-' : calculator_1.sub
-        '*' : calculator_1.mul
-        '/' : calculator_1.div
-
-        }
-    if num != 4:
-       print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-       exit(1)
+    argc = len(argv)
+    ops = {
+        '+': calculator_1.add,
+        '-': calculator_1.sub,
+        '*': calculator_1.mul,
+        '/': calculator_1.div,
+    }
+    if argc != 4:
+        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
     a = int(argv[1])
     b = int(argv[3])
-    operator = argv[2]
-
-    if operator not in '+-*/':
-        print("Unknown operator. Available operators: +, -, * and /")
+    op = argv[2]
+    if op not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
+    res = ops[op](a, b)
+    print('{:d} {:s} {:d} = {:d}'.format(a, op, b, res))
 
-    res = operators[operator](a, b)
-    print('{:d} {:s} {:d} = {:d}'.format(a, operator, b, res))
 
 if __name__ == '__main__':
-    from calculator_1
     from sys import argv, exit
+    import calculator_1
     main(argv)
+Footer
