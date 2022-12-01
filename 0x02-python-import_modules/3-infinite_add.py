3fa9f3b1aa622b8ada37ab4742ca7e44d0efc9b4
sethdanny <naddulidaniel1994@gmail.com>
2022-12-01 21:37:05 +0300

    added task 1

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    num = len(sys.argv)
+    sum = 0
+    for i in range(1, num):
+        sum = int(sys.argv[i])
+    print("{}".format(sum))
