a28cdb7ac3e8303fc2fdee05de3a4d1be0f6e869
sethdanny <naddulidaniel1994@gmail.com>
2022-12-02 01:07:57 +0300

    hello

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-def main(argv):
-    argc = len(argv)
-    ops = {
-        '+': calculator_1.add,
-        '-': calculator_1.sub,
-        '*': calculator_1.mul,
-        '/': calculator_1.div,
-    }
-    if argc != 4:
-        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
+
+    op = argv[2]
+    f = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in f:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
     a = int(argv[1])
     b = int(argv[3])
-    op = argv[2]
-    if op not in '+-*/':
-        print('Unknown operator. Available operators: +, -, * and /')
-        exit(1)
-    res = ops[op](a, b)
-    print('{:d} {:s} {:d} = {:d}'.format(a, op, b, res))
-
-
-if __name__ == '__main__':
-    from sys import argv, exit
-    import calculator_1
-    main(argv)
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
