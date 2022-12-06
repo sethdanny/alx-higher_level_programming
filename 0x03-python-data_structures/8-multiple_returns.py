2ae194319c0c6084fb980555601b2d9c1751d9b5
sethdanny <naddulidaniel1994@gmail.com>
2022-12-06 17:21:21 +0300

    fix task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        sentence[0] = None
-    else:
-        return(len(sentence), sentence[0])
+    length = len(sentence)
+    char = sentence[0] if length > 0 else None
 
+    return (length, char)
