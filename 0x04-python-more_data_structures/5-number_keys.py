a320250f6ac384a603cbd12e7995f25a4a9e809b
sethdanny <naddulidaniel1994@gmail.com>
2022-12-07 22:38:36 +0300

    counted keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    new_list = []
-    sum = 0
-    for key in a_dictionary.keys():
-        new_list.append(key)
-        sum = sum + len(new_list)
-
-    return sum
-    
+    result = len(a_dictionary.keys())
+    return result
