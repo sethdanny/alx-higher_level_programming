2a9ecbe2c09569c25346a854405df30c9f21e903
sethdanny <naddulidaniel1994@gmail.com>
2022-12-07 22:26:22 +0300

    added unique values

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_list = []
+    new_list = set(my_list)
     sum = 0
-
-    for item in range(len(my_list)):
-        if item not in unique_list:
-            unique_list.append(item)
-
-        for item in range(len(unique_list)):
-            sum = sum + unique_list[item]
+    for i in new_list:
+        sum += i
     return sum
