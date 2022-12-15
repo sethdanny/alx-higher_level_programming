8f6c478636b8aa22c595a48fa43ced602c3c5fcf
sethdanny <naddulidaniel1994@gmail.com>
2022-12-15 15:42:35 +0300

    fixed indentation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,10 @@ def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     translations = {
-        "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000
+        "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
         }
     number = 0
-    roman_string = roman_string.replace("IV", "IIII").replace("IX" , "VIII")
+    roman_string = roman_string.replace("IV", "IIII").replace("IX", "VIII")
     roman_string = roman_string.replace("XL", "XXXX").replace("XC", "LXXXX")
     roman_string = roman_string.replace("CD", "CCCC").replace("CM", "DCCCC")
 
