a42e3a14956babc7656173a459ca25e12acd952b
sethdanny <naddulidaniel1994@gmail.com>
2022-12-15 15:46:29 +0300

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
         "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000
         }
     number = 0
-    roman_string = roman_string.replace("IV", "IIII").replace("IX", "VIII")
+    roman_string = roman_string.replace("IV", "IIII").replace("IX", "VIIII")
     roman_string = roman_string.replace("XL", "XXXX").replace("XC", "LXXXX")
     roman_string = roman_string.replace("CD", "CCCC").replace("CM", "DCCCC")
 
