4a40f31118557f76a0d2a9bc486a1963351d44f4
sethdanny <naddulidaniel1994@gmail.com>
2022-12-20 00:42:54 +0300

    floats

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,5 +4,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, FloatingPointError):
         result = None
     finally:
-        print("Inside result: {:d}".format(result))
+        print("Inside result: {}".format(result))
     return result
