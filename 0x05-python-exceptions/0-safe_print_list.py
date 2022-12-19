deecd45aad679cae29f4665e36b842aa493b4e10
sethdanny <naddulidaniel1994@gmail.com>
2022-12-19 22:59:10 +0300

    added task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except:
+        except ValueError:
             pass
 
     print()
-
     return (count)
