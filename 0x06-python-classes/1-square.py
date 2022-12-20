8d08b2b4708f605787cc5a6b504cd5cfc77fcfc2
sethdanny <naddulidaniel1994@gmail.com>
2022-12-20 20:54:06 +0300

    added private size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for square class """
+
+
+class Square:
+    """ class square with constructor """
+    def __init__(self, size):
+        """ magic method """
+        self.__size = size
