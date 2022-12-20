6a596b3562cfb83cf137f66fb140c7a85ab540f4
sethdanny <naddulidaniel1994@gmail.com>
2022-12-21 01:31:22 +0300

    added task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" this module represents a class square """
+
+
+class Square:
+    """Represents a square class """
+    def __init__(self, size=0):
+        """Initialize data"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Returns the area of a square """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """ print square of using hashes """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
