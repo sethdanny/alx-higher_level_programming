d8b7111bbfb24fae920ed988a845d9dcc76f1c07
sethdanny <naddulidaniel1994@gmail.com>
2022-12-28 01:25:21 +0300

    added tasks

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
     """ function to return firstname and lastname """
-    
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
