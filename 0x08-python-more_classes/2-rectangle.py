fb2a2117c1f143a0058f1a6eed28e718e4ac4f0f
sethdanny <naddulidaniel1994@gmail.com>
2023-01-18 10:18:08 +0300

    added task 1

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+""" module for a rectangle """
+
+
+class Rectangle(object):
+    """ creates a rectangle class """
+    def __init__(self, width=0, height=0):
+        """ Initializing the data """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ sets the width of the instance """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ gets the width of a rectangle class """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ sets the height of the rectangle class """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ gets the height of the instance """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """ returns the area of a rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ prints the perimeter of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2(self.__width + self.__height)
