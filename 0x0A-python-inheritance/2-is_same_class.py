7415a55a3f9bc36f3980f507582d728db07fc12a
sethdanny <naddulidaniel1994@gmail.com>
2023-01-22 01:37:52 +0300

    added class int

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,4 @@
 
 def is_same_class(obj, a_class):
     """ returns true if an object is an instance of a class """
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
