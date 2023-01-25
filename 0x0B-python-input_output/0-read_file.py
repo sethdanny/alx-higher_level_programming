65589342915c89d3d5564a44a8ca406806e78be3
sethdanny <naddulidaniel1994@gmail.com>
2023-01-25 21:30:20 +0300

    documented

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" module for reading files """
+
+
 def read_file(filename=""):
+    """ read text files """
     with open(filename, encoding="utf-8") as f:
         print(f.read())
