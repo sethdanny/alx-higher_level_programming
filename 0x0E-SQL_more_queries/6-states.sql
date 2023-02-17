b85e86ba364ee596e453a384b3ed0ecf94350b0b
sethdanny <naddulidaniel1994@gmail.com>
2023-02-18 01:09:05 +0300

    states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create database if doesnt exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create a table within the database
+CREATE TABLE IF NOT EXISTS states
+(id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL, UNIQUE(id));
