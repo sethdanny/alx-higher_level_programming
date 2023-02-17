db095e894be0656dffbe12816d90922cb247b1c6
sethdanny <naddulidaniel1994@gmail.com>
2023-02-18 01:12:03 +0300

    hot fix

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,4 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create a table within the database
 CREATE TABLE IF NOT EXISTS states
 (id INT NOT NULL AUTO INCREMENT,
-name VARCHAR(256) NOT NULL, UNIQUE(id));
+name VARCHAR(256) NOT NULL, UNIQUE(id),
+PRIMARY KEY(id));
