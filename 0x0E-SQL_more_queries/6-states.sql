b77ccfcd8eeeb41d1cfe22a19329217fb4a0f9c0
sethdanny <naddulidaniel1994@gmail.com>
2023-02-18 01:14:15 +0300

    fix use data base

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,9 @@
 -- create database if doesnt exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use database
+USE hbtn_0d_usa;
 -- create a table within the database
 CREATE TABLE IF NOT EXISTS states
-(id INT NOT NULL AUTO INCREMENT,
+(id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL, UNIQUE(id),
 PRIMARY KEY(id));
