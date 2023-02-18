cd0270458de0de84fc05bb0542bb5e02086cab94
sethdanny <naddulidaniel1994@gmail.com>
2023-02-18 16:11:03 +0300

    added task 9

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- create a subquery of california city
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;;
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- join tables and create subqueries
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id;
