113baa59a9b7af8d87c26fd3a2bcc63a2071d9da
sethdanny <naddulidaniel1994@gmail.com>
2023-03-13 16:39:15 +0300

    print multiples

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
     console.log('Missing number of occurrences');
 }
 for (let i = 0; i < parseInt(process.argv[2]); i++) {
