1496f036a5998723b28abceeb411a72a89096056
sethdanny <naddulidaniel1994@gmail.com>
2023-03-13 17:21:24 +0300

    factorial notation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const factorial = (n) => {
-    if (n < 0) {
-        return -1;
-  } else if (n === 0 || isNaN(n)) {
-    return 1;
-  } else {
-    return (n * factorial(n - 1));
+  if (n < 0) {
+    return (-1);
   }
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
 };
 console.log(factorial(Number(process.argv[2])));
