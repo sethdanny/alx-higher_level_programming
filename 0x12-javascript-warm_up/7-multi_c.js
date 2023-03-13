8072ae0321f03a9ba304a895a80ea8afbdf98cd5
sethdanny <naddulidaniel1994@gmail.com>
2023-03-13 16:41:17 +0300

    fixed semistandard errors

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
-for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('C is fun');
+for (let i = 0; i < Number(process.argv[2]); i++) {
+  console.log('C is fun');
 }
