4c132e3cad05e6f3d556d13c40df25ea45b7094d
sethdanny <naddulidaniel1994@gmail.com>
2023-03-13 23:34:44 +0300

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const factorial = n => {
+function factorial (n) {
   if (n === 0 || isNaN(n)) {
     return (1);
   }
   return (n * factorial(n - 1));
-};
+}
 console.log(factorial(Number(process.argv[2])));
