4297106f1a95740017fbfdc851b8289f123cc94e
sethdanny <naddulidaniel1994@gmail.com>
2023-03-13 17:19:05 +0300

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const factorial = (n) => {
-  if (n < 0) {
-    return -1;
-  }
-  if (n === 0 || n === 1) {
+    if (n < 0) {
+        return -1;
+  } else if (n === 0 || isNaN(n)) {
     return 1;
   } else {
     return (n * factorial(n - 1));
