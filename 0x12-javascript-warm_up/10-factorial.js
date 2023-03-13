e56848d6c5725b41896014d6842be411af2964e9
sethdanny <naddulidaniel1994@gmail.com>
2023-03-13 17:12:26 +0300

    computed factorial using recursive method

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const factorial = (n) => {
+  if (n < 0) {
+    return -1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return (n * factorial(n - 1));
+  }
+};
+console.log(factorial(process.argv[2]));
