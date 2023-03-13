200fbc6557a420ef244b291da44c4362f9b56aa1
sethdanny <naddulidaniel1994@gmail.com>
2023-03-13 16:23:04 +0300

    added task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined) {
+    console.log('Missing number of occurrences');
+}
+for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+}
