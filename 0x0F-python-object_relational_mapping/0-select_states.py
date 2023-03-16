43c345477908094153c157b8e8a45c5504d5b8f1
sethdanny <naddulidaniel1994@gmail.com>
2023-03-16 14:22:34 +0300

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+connect to the database and fetch data 
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1], 
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+    except:
+        print("error in connection")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
