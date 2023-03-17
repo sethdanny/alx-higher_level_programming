e07a9666cbcb621203e63e242ad94000268d32c0
sethdanny <naddulidaniel1994@gmail.com>
2023-03-17 22:14:33 +0300

    added

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" create a table schema """
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+
+class States(Base):
+    """ state class for state table"""
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
