39dc56234e96765c8becf69702db0f2f00ca9752
sethdanny <naddulidaniel1994@gmail.com>
2023-03-17 10:08:14 +0300

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,20 @@
 #!/usr/bin/env python3
 """
-connect to the database and fetch data 
+connect to the database and fetch data
 """
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    try:
-        mydb = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1], 
+mydb = MySQLdb.connect(
+            host="localhost", user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306
         )
-    except:
-        print("error in connection")
-    try:
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-    except:
-        print("Error in fectching data")
-    cur.close()
-    mydb.close()
+
+cur = mydb.cursor()
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
 """
-connect to the database and fetch data 
+connect to the database and fetch data
 """
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    try:
-        mydb = MySQLdb.connect(
+mydb = MySQLdb.connect(
             host="localhost",
-            user=sys.argv[1], 
+            user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306
         )
-    except:
-        print("error in connection")
-    try:
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC ")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-    except:
-        print("Error in fectching data")
-    cur.close()
-    mydb.close()
+
+cur = mydb.cursor()
+cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+             ORDER BY states.id ASC ")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
