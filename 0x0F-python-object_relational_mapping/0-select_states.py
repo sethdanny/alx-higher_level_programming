8456164881f8bfcb0b1ad1c291d858df0cea4db8
sethdanny <naddulidaniel1994@gmail.com>
2023-03-17 10:37:35 +0300

    added

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,17 @@ connect to the database and fetch data
 import MySQLdb
 import sys
 
+if __name__ == '__main__':
 
-mydb = MySQLdb.connect(
+    mydb = MySQLdb.connect(
             host="localhost", user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306
         )
 
-cur = mydb.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    cur = mydb.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,10 +2,11 @@
 """
 connect to the database and fetch data
 """
-import MySQLdb
-import sys
 
-mydb = MySQLdb.connect(
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    mydb = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
             passwd=sys.argv[2],
@@ -13,9 +14,9 @@ mydb = MySQLdb.connect(
             port=3306
         )
 
-cur = mydb.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE BINARY'{}\
+    cur = mydb.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'{}\
             ' ORDER BY states.id ASC".format(sys.argv[4]))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
