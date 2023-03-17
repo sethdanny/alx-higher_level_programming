05715a2f5421aad924e797de68967f368e6a43a1
sethdanny <naddulidaniel1994@gmail.com>
2023-03-17 11:17:52 +0300

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""
-connect to the database and fetch data
+"""takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+where name matches the argument
 """
 
 if __name__ == '__main__':
@@ -11,12 +12,11 @@ if __name__ == '__main__':
             user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
-            port=3306
-        )
+            port=3306)
 
     cur = mydb.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'{}\
-            ' ORDER BY states.id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
