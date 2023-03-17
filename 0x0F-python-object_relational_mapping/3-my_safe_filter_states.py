67413fb4486174470ff916760f53e01178685e4e
sethdanny <naddulidaniel1994@gmail.com>
2023-03-17 11:29:33 +0300

    added

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                            db=sys.argv[3])
     cur = mydb.cursor()
     cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4]))
+                ORDER BY states.id ASC", (sys.argv[4],))
     rows = cur.fetchall()
     for row in rows:
         print(row)
