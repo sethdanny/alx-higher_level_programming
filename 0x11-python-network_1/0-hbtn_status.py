74d261f580cf8de2e5e492f7eddd9cd4335536a8
sethdanny <naddulidaniel1994@gmail.com>
2023-04-01 00:17:12 +0300

    removed whitespaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     with request.urlopen(url) as response:
         content = response.read()
         print('Body response:')
-        print('\t - type: {}'.format(type(content)))
-        print('\t - content: {}'.format(content))
-        print('\t - utf-8 content: {}'.format(content.decode('utf-8')))
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf-8 content: {}'.format(content.decode('utf-8')))
