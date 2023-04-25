f7b82ebc6950173c26a0dd261f039b0ac0749a85
sethdanny <naddulidaniel1994@gmail.com>
2023-04-25 16:23:44 +0300

    fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
 request(url, (err, res, body) => {
-    if (err) console.log(err);
+  if (err) {
+    console.log(err);
+  } else {
     console.log(JSON.parse(body).title);
-})
+  }
+});
