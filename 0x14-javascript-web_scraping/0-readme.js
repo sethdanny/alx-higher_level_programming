f5129a35506cfc08c8d8ed48d39dc5e7eb6f39c7
sethdanny <naddulidaniel1994@gmail.com>
2023-04-25 14:18:53 +0300

    read file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* script to read files */
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'uft8', (err, data) => {
+    if (err) {
+	console.log(err);
+    }
+    console.log(data);
+});
