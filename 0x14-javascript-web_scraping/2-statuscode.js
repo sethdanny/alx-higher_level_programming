88a785ab7cac328ad24aa32d23ca8a33ddbeba0e
sethdanny <naddulidaniel1994@gmail.com>
2023-04-25 15:05:34 +0300

    fixed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 
 request(process.argv[2], (err, res, body) => {
   if (err) console.log(err);
-  console.log('code:', res.code);
+  console.log('code:', res.statusCode);
 });
