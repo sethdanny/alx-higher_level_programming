d0cbde1d7f12bdbc2701a442f7f648bb26283923
sethdanny <naddulidaniel1994@gmail.com>
2023-04-25 14:33:57 +0300

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'uft8', (err, data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
