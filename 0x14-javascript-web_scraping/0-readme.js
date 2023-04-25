3beabcbb33d7489dfc4850aada5c0538c673f2ec
sethdanny <naddulidaniel1994@gmail.com>
2023-04-25 14:31:23 +0300

    read

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 fs.readFile(process.argv[2], 'uft8', (err, data) => {
   if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
