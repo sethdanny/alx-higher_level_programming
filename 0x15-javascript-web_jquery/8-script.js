d32ef0b5e781f1acbfe0397b8ddfb4622eb05551
sethdanny <naddulidaniel1994@gmail.com>
2023-05-03 16:31:43 +0300

    hot fix

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$(document).ready(
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data, status) {
+    if (status === 'success') {
+      $.each(data.results, function (index, movie) {
+        $('ul#list_movies').append('<li>' + movie.title + '</li>');
+      });
+    }
+  })
+);
