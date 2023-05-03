9915a4cb01ebcb26417331d74fdedfa2b9a7e1bc
sethdanny <naddulidaniel1994@gmail.com>
2023-05-03 13:38:49 +0300

    created a click event

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,5 @@
-const header = $('header');
-$('div#red_reader').click(() => {
-  header.css('color', '#FF0000');
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
 });
