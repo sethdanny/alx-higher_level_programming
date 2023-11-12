9b860fb05289bbb6ec79524d829d19705720148a
sethdanny <naddulidaniel1994@gmail.com>
2023-11-13 02:20:52 +0300

    removed spaces

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,19 +7,6 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    """end_characters = ['.', '?', ':']
-    new_text = ""
-    i = 0
-    while i < len(text):
-        new_text += text[i]
-        if text[i] in end_characters:
-            new_text += "\n\n"
-            i += 1
-            while text[i] == " ":
-                i += 1
-            continue
-        i += 1
-    print(new_text, end="") """
     text = text.strip()
     for char in text:
         print(char, end="")
