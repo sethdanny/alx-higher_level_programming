74deb63174f3ad892dde0fcb5d56c8980347b793
sethdanny <naddulidaniel1994@gmail.com>
2023-11-13 12:16:20 +0300

    passed one test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,7 +7,8 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
         def test_max_integer(self):
-                pass
+                self.assertEqual(max_integer([1,2,3,4]), 4)
+                self.assertEqual(max_integer([1,3, 2, 5]), 5)
         
 if __name__ == '__main__':
         unittest.main()
