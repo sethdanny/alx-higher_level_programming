0e73f436c7900b77c91256a8d8099691acf99a93
sethdanny <naddulidaniel1994@gmail.com>
2023-11-13 12:09:41 +0300

    bare minimum test to pass

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""unittest for max_integer([...])"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+        def test_max_integer(self):
+                pass
+        
+if __name__ == '__main__':
+        unittest.main()
