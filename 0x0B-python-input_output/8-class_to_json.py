4727f68ab2e049939f86d04248c26bf1d8ddef5d
sethdanny <naddulidaniel1994@gmail.com>
2023-11-15 00:28:02 +0300

    created class description

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean) for JSON serialization of an
+object"""
+
+def class_to_json(obj):
+    """returns object description"""
+    return obj.__dict__
